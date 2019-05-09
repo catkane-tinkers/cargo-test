c5431ddff392245bac9c74c5da11ae6d70139c64
catkane-tinkers <47026490+catkane-tinkers@users.noreply.github.com>
2019-05-09 15:21:23 +0800

    Update popping.js

--- popping.js
+++ popping.js
@@ -53,8 +53,8 @@ function draw() {
 			img,
 			Math.ceil(Math.random() * canvas_width * 0.8 + canvas_width * 0.1),
 			Math.ceil(Math.random() * canvas_height * 0.8 + canvas_height * 0.1),
-			width=80,
-			height=80
+			width=50,
+			height=50
 		);
 		number_drawn += 1;
 	}
